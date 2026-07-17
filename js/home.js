a6a0fbe4ad9db6a0c133f2101126363db24dae80
Phuggie <phuggie@phuggie.com>
2026-07-17 10:04:22 -0500

    listener update

--- js/home.js
+++ js/home.js
@@ -1,7 +1,8 @@
 window.addEventListener('load', () => {
-  positionEmbed();
-  updateTwitchEmbed();
-  typeWriter(document.querySelector('.intro h1'), 'twitch.tv/Phuggie', 80);
+    positionDropdown();
+    positionEmbed();
+    updateTwitchEmbed();
+    typeWriter(document.querySelector('.intro h1'), 'twitch.tv/Phuggie', 80);
 });
 
 window.addEventListener('resize', positionEmbed);
